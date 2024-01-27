2949bc2fb2b59cffd4397a006d7378dfe42aabd6
PotatOTheReaal <157891250+PotatOTheReaal@users.noreply.github.com>
2024-01-27 19:09:14 +0100

    Add files via upload

---
+++ calculater.py
@@ -0,0 +1,17 @@
+def main():
+    num1=float(input("Type number 1 : "))
+    num2=float(input("Type number 2 : "))
+    sign=str(input("choose you signe : "))
+    if sign=="+":
+        print(num1+num2)
+    elif sign=="-":
+        print(num1-num2)
+    elif sign=="*":
+        print(num1 * num2)
+    elif sign=="/":
+        print(num1/num2)
+
+main()
+
+
+
